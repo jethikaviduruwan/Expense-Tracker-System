11a81e0f684652c25c10fedbee5fd6e914cb9401
Jethika Wanniarachchi <49968486+jethikaviduruwan@users.noreply.github.com>
2025-01-20 21:49:55 +0530

    Configure DB connection

--- Expense Tracker/Models/Transaction.cs
+++ Expense Tracker/Models/Transaction.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Expense_Tracker.Models
 {
@@ -9,11 +11,12 @@ namespace Expense_Tracker.Models
 
         //Category ID
 
-        public int Amount { get; set; }
-
         public int categoryIdId { get; set; }
         public Category Category { get; set; }
-        
+
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
         public string? Note { get; set; }
 
         public DateTime DateTime { get; set; } = DateTime.Now;
